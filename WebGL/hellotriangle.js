c52ec678cde5aa8f5834f6a50d2ee88d2082768f
sensival <alstjs883@naver.com>
2024-08-01 17:56:23 +0900

    블록 쌓기

--- WebGL/hellotriangle.js
+++ WebGL/hellotriangle.js
@@ -74,7 +74,7 @@ var VSHADER_SOURCE =
 // Fragment shader program
 var FSHADER_SOURCE =
   'void main() {\n' +
-  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
+  '  gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);\n' +
   '}\n';
 
 function main() {
